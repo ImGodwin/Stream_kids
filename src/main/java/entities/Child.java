11c1afb488d859bf08e17d20b8c7953c94870a31
Godwin <godwinmail6@gmail.com>
2024-02-02 23:33:35 +0100

    created child class

--- src/main/java/entities/Child.java
+++ src/main/java/entities/Child.java
@@ -1,4 +1,20 @@
 package entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class Child {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String surname;
+
 }
