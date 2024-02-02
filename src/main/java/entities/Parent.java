60d89cb213517ea79a8153dc874acb2784d686e2
Godwin <godwinmail6@gmail.com>
2024-02-02 23:32:11 +0100

    Added annotations

--- src/main/java/entities/Parent.java
+++ src/main/java/entities/Parent.java
@@ -2,9 +2,12 @@ package entities;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
-
+@Getter
+@Setter
 public class Parent {
 
     @Id
